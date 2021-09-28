7fa593dcc294b0a0ee2b64624b19671815a65536
Amy Pellegrini <amyvpellegrini@gmail.com>
2021-09-28 16:04:01 +0100

    Render staff and clef / code gen proof of concept

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Staff, Clef } from "react-scorewriter";
 
 const ReactMusicXML = ({ content }: { content: string }) => {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(content, "text/xml");
+  const rootElement = xmlDoc.children[0];
+  const parts = Array.from(rootElement.getElementsByTagName("part"));
 
-  console.log(xmlDoc);
+  return (
+    <svg>
+      {parts.map((part, idx) => {
+        const clef = part.children[0].children[0].getElementsByTagName("clef");
+        const clefType =
+          clef[0].getElementsByTagName("sign")[0].textContent === "G"
+            ? "G-clef"
+            : "F-clef";
 
-  return <></>;
+        return (
+          <>
+            <Clef name={clefType} />
+            <Staff key={idx} />
+          </>
+        );
+      })}
+    </svg>
+  );
 };
 
 export default ReactMusicXML;
